1bc3a08e29637d74967cbf99205a7966af5b87e8
seriusanbudi <budiharta_21@live.com>
2020-11-07 11:11:41 +0800

    :sparkles: move head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import Nav from '../components/ui/Nav'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
+    <Head>
+      <title>Awesome Frontend Dev Here!</title>
+
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
+    </Head>
     <Nav />
 
     <Component {...pageProps} />
